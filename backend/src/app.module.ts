c1e0af29255914f9dc1b58c091a5b9e6adb39129
leandro8279 <leandro8279@gmail.com>
2026-05-14 18:36:36 -0300

    feat: adicionado middleware de log de requisição e configuração dos módulos do NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RequestLoggerMiddleware } from '@/common/middlewares/RequestLogger.middleware';
+import { AuthModule } from '@/modules/auth/auth.module';
+import { DatabaseModule } from '@/shared/database/database.module';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // load: [appConfig, databaseConfig, jwtConfig, cacheConfig],
+    }),
+    DatabaseModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
